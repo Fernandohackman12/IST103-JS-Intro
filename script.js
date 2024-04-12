e6520901a8ca893258e030a273c31da408d89b13
fernando12 <lic-fernando12@outlook.com>
2024-04-11 23:50:23 -0700

    final changes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert("Hello, world!");
+// alert("Hello, world!"); //
+//Activity 2//
+let personName = "Alberto";
+let personAge = 31;
+let Ismarried = true;
+let IsEmployed = false;
+
+console.log("A random person name is:" + personName);
+console.log("A person age is:" + personAge);
+console.log("A person age is:" + personAge);
+let;
+
+//Activity 3//
+let todoList = ("Go to the doctor", "do final project", "learn russian");
+
+let myBicycle= (
+    Brand: "RadPowerbikes",
+    Color: "Black",
+    isUsed: True,
+    numberOfbatteries: 2,   
+)
+
+console.log ("my first to do is:"+ todoList)
+console.log ("my bicycle is:"+ myBicycle)
